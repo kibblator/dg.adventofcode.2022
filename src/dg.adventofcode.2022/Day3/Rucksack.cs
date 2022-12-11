8058e476aec67410d98f108db059bbefb0e4ce95
Daniel Gregson <dpgregson@hotmail.com>
2022-12-11 20:11:15 +0000

    chore: completed day 3

---
+++ src/dg.adventofcode.2022/Day3/Rucksack.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace dg.adventofcode._2022.Day3;
+
+public static class Rucksack
+{
+    private const int LowercaseCharOffset = 96;
+    private const int UpperCaseCharOffset = 38;
+    
+    public static int GetPrioritySum(List<string> input)
+    {
+        var sumOfAll = 0;
+        foreach (var rucksack in input)
+        {
+            var halfLength = rucksack.Length / 2;
+            var half1 = rucksack.Substring(0, halfLength);
+            var half2 = rucksack.Substring(halfLength, halfLength);
+
+            var sharedValues = half1.Intersect(half2).ToList();
+
+            foreach (var sharedValue in sharedValues)
+            {
+                if (sharedValue.ToString() == sharedValue.ToString().ToLower())
+                    sumOfAll += sharedValue - LowercaseCharOffset;
+                else
+                {
+                    sumOfAll += sharedValue - UpperCaseCharOffset;
+                }
+            }
+        }
+        
+        return sumOfAll;
+    }
+
+    public static int GetPriorityGroupsSum(List<string> input)
+    {
+        var sumOfAll = 0;
+        
+        for (var i = 0; i <  input.Count; i+=3)
+        {
+            var group1 = input[i];
+            var group2 = input[i + 1];
+            var group3 = input[i + 2];
+
+            var sharedValues = group1.Intersect(group2).Intersect(group3).ToList();
+
+            foreach (var sharedValue in sharedValues)
+            {
+                if (sharedValue.ToString() == sharedValue.ToString().ToLower())
+                    sumOfAll += sharedValue - LowercaseCharOffset;
+                else
+                {
+                    sumOfAll += sharedValue - UpperCaseCharOffset;
+                }
+            }
+        }
+
+        return sumOfAll;
+    }
+}
---
+++ tests/dg.adventofcode.2022.tests/Day3/RucksackTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using dg.adventofcode._2022.crosscutting;
+using dg.adventofcode._2022.Day3;
+using NUnit.Framework;
+
+namespace dg.adventofcode._2022.tests.Day3;
+
+public class RucksackTests
+{
+    [Test]
+    public void TestData()
+    {
+        var input = new List<string>
+        {
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw"
+        };
+
+        const int expectedResult = 157;
+        var result = Rucksack.GetPrioritySum(input);
+        
+        Assert.AreEqual(expectedResult, result);
+    }
+
+    [Test]
+    public void Part1()
+    {
+        var textFileLoader = new TextFileLoader();
+        var input = textFileLoader.LoadStringListFromStrings("TestData\\Rucksack.txt");
+        
+        var result = Rucksack.GetPrioritySum(input);
+        Console.WriteLine(result);
+    }
+    
+    [Test]
+    public void Part2_Example()
+    {
+        var expectedResult = 70;
+        var input = new List<string>
+        {
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw"
+        };
+        
+        var result = Rucksack.GetPriorityGroupsSum(input);
+        Assert.AreEqual(expectedResult, result);
+    }
+    
+    [Test]
+    public void Part2()
+    {
+        var textFileLoader = new TextFileLoader();
+        var input = textFileLoader.LoadStringListFromStrings("TestData\\Rucksack.txt");
+        
+        var result = Rucksack.GetPriorityGroupsSum(input);
+        Console.WriteLine(result);
+    }
+}
