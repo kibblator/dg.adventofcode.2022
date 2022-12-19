1bee6fc4b881ae83d9ae0ed09b6ba357bd77bbbd
Daniel Gregson <dpgregson@hotmail.com>
2022-12-19 16:06:31 +0000

    chore: completed Day 4

---
+++ src/dg.adventofcode.2022/Day4/CampCleanup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace dg.adventofcode._2022.Day4;
+
+public static class CampCleanup
+{
+    public static int GetContainingRanges(List<string> input)
+    {
+        var groupsOfNote = 0;
+        foreach (var line in input)
+        {
+            var rangeParts = line.Split(',');
+            var firstRange = ExtractRangeFromString(rangeParts[0]);
+            var secondRange = ExtractRangeFromString(rangeParts[1]);
+            var firstGroup = Enumerable.Range(firstRange.startIndex, firstRange.length).ToList();
+            var secondGroup = Enumerable.Range(secondRange.startIndex, secondRange.length).ToList();
+
+            if (!firstGroup.Except(secondGroup).Any() || !secondGroup.Except(firstGroup).Any())
+                groupsOfNote++;
+        }
+        
+        return groupsOfNote;
+    }
+    
+    public static int GetOverlappingRanges(List<string> input)
+    {
+        var groupsOfNote = 0;
+        foreach (var line in input)
+        {
+            var rangeParts = line.Split(',');
+            var firstRange = ExtractRangeFromString(rangeParts[0]);
+            var secondRange = ExtractRangeFromString(rangeParts[1]);
+            var firstGroup = Enumerable.Range(firstRange.startIndex, firstRange.length).ToList();
+            var secondGroup = Enumerable.Range(secondRange.startIndex, secondRange.length).ToList();
+
+            if (firstGroup.Any(fg => secondGroup.Contains(fg)))
+                groupsOfNote++;
+        }
+        
+        return groupsOfNote;
+    }
+
+    private static (int startIndex, int length) ExtractRangeFromString(string rangeString)
+    {
+        var startIndex = Convert.ToInt32(rangeString.Split('-')[0]);
+        var length = Convert.ToInt32(rangeString.Split('-')[1]) - startIndex + 1;
+        return (startIndex, length);
+    }
+}
---
+++ tests/dg.adventofcode.2022.tests/Day4/CampCleanupTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using dg.adventofcode._2022.crosscutting;
+using dg.adventofcode._2022.Day4;
+using NUnit.Framework;
+
+namespace dg.adventofcode._2022.tests.Day4;
+
+public class CampCleanupTests
+{
+    [Test]
+    public void Part_Example()
+    {
+        var input = new List<string>
+        {
+            "2-4,6-8",
+            "2-3,4-5",
+            "5-7,7-9",
+            "2-8,3-7",
+            "6-6,4-6",
+            "2-6,4-8"
+        };
+
+        const int expectedResult = 2;
+        var result = CampCleanup.GetContainingRanges(input);
+        
+        Assert.AreEqual(expectedResult, result);
+    }
+
+    [Test]
+    public void Part1()
+    {
+        var textFileLoader = new TextFileLoader();
+        var input = textFileLoader.LoadStringListFromStrings("TestData\\CampCleanup.txt");
+        
+        var result = CampCleanup.GetContainingRanges(input);
+        Console.WriteLine(result);
+    }
+    
+    [Test]
+    public void Part2_Example()
+    {
+        var expectedResult = 4;
+        var input = new List<string>
+        {
+            "2-4,6-8",
+            "2-3,4-5",
+            "5-7,7-9",
+            "2-8,3-7",
+            "6-6,4-6",
+            "2-6,4-8"
+        };
+        
+        var result = CampCleanup.GetOverlappingRanges(input);
+        Assert.AreEqual(expectedResult, result);
+    }
+    
+    [Test]
+    public void Part2()
+    {
+        var textFileLoader = new TextFileLoader();
+        var input = textFileLoader.LoadStringListFromStrings("TestData\\CampCleanup.txt");
+        
+        var result = CampCleanup.GetOverlappingRanges(input);
+        Console.WriteLine(result);
+    }
+}
