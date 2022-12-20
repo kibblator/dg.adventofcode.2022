57e0682a3bb90b439eaa002a572476dd5ae0843b
Daniel Gregson <dpgregson@hotmail.com>
2022-12-20 17:58:49 +0000

    chore: completed Day 8 Part 2

--- src/dg.adventofcode.2022/Day8/TreeHouse.cs
+++ src/dg.adventofcode.2022/Day8/TreeHouse.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 
 namespace dg.adventofcode._2022.Day8;
 
@@ -37,7 +38,31 @@ public static class TreeHouse
 
     public static double GetBestScenicScore(List<string> input)
     {
-        return 0;
+        var width = input.First().Length;
+        var length = input.Count;
+
+        var matrix = new int[width, length];
+        PopulateMatrix(matrix, input);
+
+        var scenicScoreList = new List<ScenicScore>();
+        
+        for (var i = 1; i < width - 1; i++)
+        {
+            for (var j = 1; j < length - 1; j++)
+            {
+                var treeValue = matrix[j, i];
+                var scenicScore = new ScenicScore
+                {
+                    DownScore = GatherScenicScore(treeValue, matrix, width, length, i, j, CheckDir.Down),
+                    LeftScore = GatherScenicScore(treeValue, matrix, width, length, i, j, CheckDir.Left),
+                    UpScore = GatherScenicScore(treeValue, matrix, width, length, i, j, CheckDir.Up),
+                    RightScore = GatherScenicScore(treeValue, matrix, width, length, i, j, CheckDir.Right)
+                };
+                scenicScoreList.Add(scenicScore);
+            }
+        }
+        
+        return scenicScoreList.Max(s => s.TotalScore());
     }
 
     private static void PopulateMatrix(int[,] matrix, List<string> input)
@@ -95,6 +120,70 @@ public static class TreeHouse
                 throw new ArgumentOutOfRangeException(nameof(checkDir), checkDir, null);
         }
     }
+    
+    private static double GatherScenicScore(int treeValue, int[,] matrix, int width, int length, int i, int j, CheckDir checkDir)
+    {
+        var counter = 0;
+        switch (checkDir)
+        {
+            case CheckDir.Up:
+                j--;
+                while (j >= 0)
+                {
+                    if (treeValue <= matrix[j, i])
+                    {
+                        counter++;
+                        return counter;
+                    }
+
+                    counter++;
+                    j--;
+                }
+
+                return counter;
+            case CheckDir.Down:
+                j++;
+                while (j < length)
+                {
+                    if (treeValue <= matrix[j, i])
+                    {
+                        counter++;
+                        return counter;
+                    }
+                    counter++;
+                    j++;
+                }
+                return counter;
+            case CheckDir.Left:
+                i--;
+                while (i >= 0)
+                {
+                    if (treeValue <= matrix[j, i])
+                    {
+                        counter++;
+                        return counter;
+                    }
+                    counter++;
+                    i--;
+                }
+                return counter;
+            case CheckDir.Right:
+                i++;
+                while (i < width)
+                {
+                    if (treeValue <= matrix[j, i])
+                    {
+                        counter++;
+                        return counter;
+                    }
+                    counter++;
+                    i++;
+                }
+                return counter;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(checkDir), checkDir, null);
+        }
+    }
 }
 
 internal enum CheckDir
@@ -103,4 +192,17 @@ internal enum CheckDir
     Down = 2,
     Left = 3,
     Right = 4
+}
+
+internal class ScenicScore
+{
+    public double UpScore { get; set; }
+    public double DownScore { get; set; }
+    public double LeftScore { get; set; }
+    public double RightScore { get; set; }
+
+    public double TotalScore()
+    {
+        return UpScore * LeftScore * RightScore * DownScore;
+    }
 }
