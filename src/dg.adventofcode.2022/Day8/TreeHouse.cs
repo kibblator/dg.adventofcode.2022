37986926988a44ba4b9d68d133b94fa25af30715
Daniel Gregson <dpgregson@hotmail.com>
2022-12-20 16:40:34 +0000

    chore: setup Day 8

---
+++ src/dg.adventofcode.2022/Day8/TreeHouse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace dg.adventofcode._2022.Day8;
+
+public static class TreeHouse
+{
+    public static int GetVisibleTrees(List<string> input)
+    {
+        return 0;
+    }
+}
---
+++ tests/dg.adventofcode.2022.tests/Day8/TreeHouseTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using dg.adventofcode._2022.crosscutting;
+using dg.adventofcode._2022.Day8;
+using NUnit.Framework;
+
+namespace dg.adventofcode._2022.tests.Day8;
+
+public class TreeHouseTests
+{
+    [Test]
+    public void Part1_Example()
+    {
+        const int expectedResult = 21;
+        var input = new List<string>
+        {
+            "30373",
+            "25512",
+            "65332",
+            "33549",
+            "35390"
+        };
+        
+        var result = TreeHouse.GetVisibleTrees(input);
+        
+        Assert.AreEqual(expectedResult, result);
+    }
+
+    [Test]
+    public void Part1()
+    {
+        var textFileLoader = new TextFileLoader();
+        var input = textFileLoader.LoadStringListFromStrings("TestData\\TreeHouse.txt");
+        
+        var result = TreeHouse.GetVisibleTrees(input);
+        Console.WriteLine(result);
+    }
+    
+    // [Test]
+    // public void Part2_Example()
+    // {
+    //     const int expectedResult = 21;
+    //     var input = new List<string>
+    //     {
+    //         "30373",
+    //         "25512",
+    //         "65332",
+    //         "33549",
+    //         "35390"
+    //     };
+    //     var result = TreeHouse.GetVisibleTrees(input);
+    //     
+    //     Assert.AreEqual(expectedResult, result);
+    // }
+    //
+    // [Test]
+    // public void Part2()
+    // {
+    //     var textFileLoader = new TextFileLoader();
+    //     var input = textFileLoader.LoadStringListFromStrings("TestData\\TreeHouse.txt");
+    //     
+    //     var result = TreeHouse.GetVisibleTrees(input);
+    //     Console.WriteLine(result);
+    // }
+}
